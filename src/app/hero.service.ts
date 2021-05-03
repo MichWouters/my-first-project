ac6bd2572df875a41965411b737c69db33c87aff
Michiel Wouters <michiel_woute64@hotmail.com>
2021-05-03 14:50:07 +0200

    Added exampleDb

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,20 +9,19 @@ import { MessagesService } from './messages.service';
 })
 export class HeroService {
   constructor(private messagesService: MessagesService, private http: HttpClient) { }
-    private heroesUrl = 'api/heroes';
+  private heroesUrl = 'api/heroes2';
 
+  // Typescrips voor: Task<Hero> GetHero(int id) { ... }
+  getHero(id: number): Observable<Hero> {
+    // Make HTTP API call -> Get single Hero from specified URL
+    const url = `${this.heroesUrl}/${id}`;
+    const hero = this.http.get<Hero>(url);
 
-    // Typescrips voor: Task<Hero> GetHero(int id) { ... }
-    getHero(id: number): Observable<Hero> {
-      // Make HTTP API call -> Get single Hero from specified URL
-      const url = `${this.heroesUrl}/${id}`;
-      const hero = this.http.get<Hero>(url);
+    this.messagesService.addMessage(`HeroService: Fetched hero id: ${id}`);
+    return hero;
+  }
 
-      this.messagesService.addMessage(`HeroService: Fetched hero id: ${id}`);
-      return hero;
-    }
-
-  getHeroes(): Observable<Hero[]>{
+  getHeroes(): Observable<Hero[]> {
     // Make HTTP API call -> Get array of Heroes from specified URL
     const heroes = this.http.get<Hero[]>(this.heroesUrl);
 
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -20,11 +20,21 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 20, name: 'Tornado', imagePath: "http://placekitten.com/400/600" },
     ];
 
-    return { heroes };
+    const heroes2: Hero[] = [
+      { id: 11, name: 'Dr. Not Nice', imagePath: "http://placekitten.com/400/600" },
+      { id: 12, name: 'Narco', imagePath: "http://placekitten.com/400/600" },
+      { id: 13, name: 'Bombasto', imagePath: "http://placekitten.com/400/600" },
+      { id: 14, name: 'Celeritas', imagePath: "http://placekitten.com/400/600" },
+      { id: 15, name: 'Magneta', imagePath: "http://placekitten.com/400/600" },
+      { id: 16, name: 'RubberMan', imagePath: "http://placekitten.com/400/600" },
+      { id: 17, name: 'Dynama', imagePath: "http://placekitten.com/400/600" },
+      { id: 18, name: 'Dr. IQ', imagePath: "http://placekitten.com/400/600" },
+      { id: 19, name: 'Magma', imagePath: "http://placekitten.com/400/600" },
+      { id: 20, name: 'Tornado', imagePath: "http://placekitten.com/400/600" },
+    ];
+
+    return { heroes, heroes2 };
   }
-
-
-
 
   constructor() { }
 }
