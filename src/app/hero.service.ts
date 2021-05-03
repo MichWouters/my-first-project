8a1bc38764c33c549cb77075131826b96777c047
Michiel Wouters <michiel_woute64@hotmail.com>
2021-05-03 14:44:24 +0200

    All data now comes from API

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { MessagesService } from './messages.service';
-import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  
-  // Typescrips voor: Task<Hero> GetHero(int id) { ... }
-  getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(x => x.id === id) as Hero;
-    this.messagesService.addMessage(`HeroService: Fetched hero id: ${id}`);
+  constructor(private messagesService: MessagesService, private http: HttpClient) { }
+    private heroesUrl = 'api/heroes';
 
-    return of(hero);
-  }
 
-  constructor(private messagesService: MessagesService) { }
+    // Typescrips voor: Task<Hero> GetHero(int id) { ... }
+    getHero(id: number): Observable<Hero> {
+      // Make HTTP API call -> Get single Hero from specified URL
+      const url = `${this.heroesUrl}/${id}`;
+      const hero = this.http.get<Hero>(url);
+
+      this.messagesService.addMessage(`HeroService: Fetched hero id: ${id}`);
+      return hero;
+    }
 
   getHeroes(): Observable<Hero[]>{
-    const heroes = of(HEROES);
+    // Make HTTP API call -> Get array of Heroes from specified URL
+    const heroes = this.http.get<Hero[]>(this.heroesUrl);
+
     this.messagesService.addMessage('HeroService: Fetched heroes');
     return heroes;
   }
