1a3132054d01ac2de28890821d716360b76d099f
Michiel Wouters <michiel_woute64@hotmail.com>
2021-05-06 10:05:16 +0200

    Start of Exercise

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,9 +10,9 @@ import { MessagesService } from './messages.service';
 export class HeroService {
   constructor(private messagesService: MessagesService, private http: HttpClient) { }
   private heroesUrl = 'api/heroes';
-  
+
   httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json'})
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
   // Typescrips voor: Task<Hero> GetHero(int id) { ... }
@@ -31,7 +31,7 @@ export class HeroService {
     return this.http.get<Hero[]>(this.heroesUrl);
   };
 
-  addHero(hero: Hero) : Observable<Hero>{
+  addHero(hero: Hero): Observable<Hero> {
     this.messagesService.addMessage(`HeroService: Add hero: ${hero.name}`);
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions);
   };
@@ -39,12 +39,23 @@ export class HeroService {
   updateHero(hero?: Hero): Observable<any> {
     // Put -> Update existing data
     this.messagesService.addMessage(`HeroService: Update hero: ${hero?.name}`);
-     return this.http.put(this.heroesUrl, hero, this.httpOptions);
+    return this.http.put(this.heroesUrl, hero, this.httpOptions);
   };
 
-  deleteHero(hero: Hero): Observable<Hero>{
+  deleteHero(hero: Hero): Observable<Hero> {
     const url = `${this.heroesUrl}/${hero.id}`;
     this.messagesService.addMessage(`HeroService: Delete hero: ${hero.name}`);
     return this.http.delete<Hero>(url, this.httpOptions);
   };
+
+  searchHeroes(query: string): Observable<Hero[]> {
+    if (!query.trim()) { return of([]); }
+
+    let users = this.http.get<Hero[]>(`${this.heroesUrl}/?name=${query}`);
+    users
+      ? this.messagesService.addMessage('HeroService: Users found')
+      : this.messagesService.addMessage('HeroService: No Users found');
+
+    return users;
+  }
 }
--- src/app/superpower.service.ts
+++ src/app/superpower.service.ts
@@ -12,31 +12,35 @@ export class SuperpowerService {
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
+  
   constructor(private http: HttpClient, private messageService: MessagesService) { }
 
   getSuperPowers(): Observable<SuperPower[]> {
     const superpowers = this.http.get<SuperPower[]>(this.superpowersUrl);
     this.messageService.addMessage("SuperPowerService: Fetched superpowers");
     return superpowers;
-  }
+  };
 
   getSuperPower(id: number): Observable<SuperPower> {
     const url = `${this.superpowersUrl}/${id}`;
     const superpower = this.http.get<SuperPower>(url);
     this.messageService.addMessage(`SuperPowerService: Fetched superpower with id: ${id}`);
     return superpower;
-  }
+  };
+
   updateSuperpower(superpower?: SuperPower): Observable<any> {
     this.messageService.addMessage(`SuperpowerService: Updated superpower: ${superpower?.Name}`);
     return this.http.put(this.superpowersUrl, superpower, this.httpOptions);
-  }
+  };
+
   addSuperpower(superpower: SuperPower): Observable<SuperPower> {
     this.messageService.addMessage(`SuperPowerService: Added superpower: ${superpower.Name}`);
     return this.http.post<SuperPower>(this.superpowersUrl, superpower, this.httpOptions);
-  }
+  };
+
   deleteSuperpower(id: Number): Observable<SuperPower> {
     const url = `${this.superpowersUrl}/${id}`;
     this.messageService.addMessage(`SuperPowerService: Deleted superpower: ${id}`);
     return this.http.delete<SuperPower>(url, this.httpOptions);
-  }
+  };
 }
